ceb3871ac6709e62804f9de69e536ea19538b90c
jirawan.c <jirawan.c@kbtg.tech>
2022-02-18 15:07:45 +0700

    add demo service

--- day02/src/main/java/com/example/day02/DemoController.java
+++ day02/src/main/java/com/example/day02/DemoController.java
@@ -1,4 +1,20 @@
 package com.example.day02;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
 public class DemoController {
+
+    @Autowired
+    private DemoService demoService;
+
+    @GetMapping("/demo/{id}")
+    public DemoResponse xxxx(@PathVariable int id) {
+        demoService.process(id);
+        return new DemoResponse();
+    }
+
 }
