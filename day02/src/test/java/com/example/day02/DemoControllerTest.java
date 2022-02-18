930176befa5ffe9cc823060372641645b9e08d84
jirawan.c <jirawan.c@kbtg.tech>
2022-02-18 16:53:00 +0700

    Working mockito

--- day02/src/test/java/com/example/day02/DemoControllerTest.java
+++ day02/src/test/java/com/example/day02/DemoControllerTest.java
@@ -7,6 +7,7 @@ import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.boot.test.web.client.TestRestTemplate;
 
 import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.when;
 
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 class DemoControllerTest {
@@ -18,12 +19,24 @@ class DemoControllerTest {
     private DemoService demoService;
 
     @Test
-    void xxxx() {
+    void case_iput_1_should_return_result_with_id_equal_1() {
         // call API
+        // create stub
+        when(demoService.process(1)).thenReturn("Result with id = 1");
         DemoResponse result = restTemplate.getForObject("/demo/1", DemoResponse.class);
         //Verify result
         assertEquals(1,result.getCode());
         assertEquals("Result with id = 1" ,result.getMessage());
+    }
 
+    @Test
+    void case_iput_500_should_return_err() {
+        // call API
+        // create stub
+        when(demoService.process(500)).thenReturn("error cannot process");
+        DemoResponse result = restTemplate.getForObject("/demo/500", DemoResponse.class);
+        //Verify result
+        assertEquals(500,result.getCode());
+        assertEquals("error cannot process" ,result.getMessage());
     }
 }
